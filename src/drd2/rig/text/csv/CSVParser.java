920f1c63df1c3da9db6df94a554d22434c5f2b34
Adam Bajger <xbajger@fi.muni.cz>
2019-03-28 23:58:24 +0100

    removed method for parsing one line of CSV

--- src/drd2/rig/text/csv/CSVParser.java
+++ src/drd2/rig/text/csv/CSVParser.java
@@ -33,8 +33,49 @@ public class CSVParser {
         try {
             br = new BufferedReader(new InputStreamReader(new FileInputStream(csvFilePath), StandardCharsets.UTF_8));
             line = br.readLine(); // load the headers of the table
-            // find out which column contains rarity
 
+            byte iName;
+            byte iItemType;
+            byte iType;
+            byte iHands;
+            byte iCost;
+            byte iRarity;
+            byte iRegion;
+            byte iBonus;
+            byte iDescription;
+
+            String[] splitRow = line.split(SEPARATOR, -1);
+            for (byte i = 0; i < splitRow.length; i++) {
+                switch (splitRow[i]) {
+                    case LABEL_NAME:
+                        iName = i;
+                        break;
+                    case LABEL_ITEM_TYPE:
+                        iItemType = i;
+                        break;
+                    case LABEL_TYPE:
+                        iType = i;
+                        break;
+                    case LABEL_HANDS:
+                        iHands = i;
+                        break;
+                    case LABEL_COST:
+                        iCost = i;
+                        break;
+                    case LABEL_RARITY:
+                        iRarity = i;
+                        break;
+                    case LABEL_REGION:
+                        iRegion = i;
+                        break;
+                    case LABEL_BONUS:
+                        iBonus = i;
+                        break;
+                    case LABEL_DESCRIPTION:
+                        iDescription = i;
+                        break;
+                }
+            }
 
 
             // this will read lines one by one until end of file
@@ -55,22 +96,4 @@ public class CSVParser {
         }
         return null;
     }
-
-    public Item parseItemFromLine(String line) {
-        byte iName;
-        byte iItemType;
-        byte iType;
-        byte iHands;
-        byte iCost;
-        byte iRarity;
-        byte iRegion;
-        byte iBonus;
-        byte iDescription;
-
-        String[] splitRow = line.split(SEPARATOR, -1);
-        for (int i = 0; i < splitRow.length; i++) {
-
-        }
-    }
-
 }
